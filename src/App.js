57d296b00733fb7f0c40b40e4c8e9b0125ae07c4
whitevolfe <azeemshanr@gmail.com>
2021-12-19 19:30:01 +0530

    sample commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="main">
+      <div className="sub-main">
+      <form className>
+    <h3 className="title">Hello App</h3>
+
+    <div className="form-group">
+                    <label>Email address</label>
+                    <input type="email" className="form-control" placeholder="Enter email" />
+                </div>
+
+                <div className="form-group">
+                    <label>Password</label>
+                    <input type="password" className="form-control" placeholder="Enter password" />
+                </div>
+
+                <div className="form-group">
+                    <div className="custom-control custom-checkbox">
+                        <input type="checkbox" className="custom-control-input" id="customCheck1" />
+                        <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
+                    </div>
+                </div>
+
+                <button type="submit" className="btn btn-primary btn-block">Submit</button>
+                <p className="forgot-password text-right">
+                    Forgot <a href="#">password?</a>
+                </p>
+</form>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
